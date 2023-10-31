dd4ea039727af0bde9c672f01dc0b4ed1a6d9026
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-31 17:37:09 +0530

    Auto push

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # terraform-loadbalancers
+
+This is the root module to create Both public and Private Load Balancer.
+
+Entire backend code will be sourced from the backend module
+
+```
+https://github.com/jogendra-b54/terraform-loadbalancers.git
+```
---
+++ main.tf
@@ -0,0 +1,16 @@
+module "alb-public" {
+  source                    = "./vendor/modules/alb"
+  ENV                       = var.ENV
+  ALB_NAME                  = "roboshop-public-alb"
+  INTERNAL                  = false
+}
+
+module "alb-private" {
+  source                    = "./vendor/modules/alb"
+  ENV                       = var.ENV
+  ALB_NAME                  = "roboshop-private-alb"
+  INTERNAL                  = false
+}
+
+// You cannot variablize this piece of code in the source parameter
+
